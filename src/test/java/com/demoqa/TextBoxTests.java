548205236a8a1232c9b538863088d371ee2b571f
svasenkov <aanher@gmail.com>
2022-09-15 21:45:40 +0300

    added Text box test

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,38 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        String name = "Egor";
+
+        open("/text-box");
+//        $("[id=userName]").setValue("Egor");
+        $("#userName").setValue(name);
+        $("#userEmail").setValue("Egor@egor.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text(name));
+        $("#output #email").shouldHave(text("Egor@egor.com"));
+        $("#output #currentAddress").shouldHave(text("Some address 1"));
+        $("#output #permanentAddress").shouldHave(text("Another address 1"));
+
+    }
+}
